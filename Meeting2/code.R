d36ccd99d2406c119ee93bb0b1ec48c7e9f8134f
NogDaw <nelonyt@gmail.com>
2026-05-30 02:34:05 +0200

    Update code for QQplots

--- Meeting2/code.R
+++ Meeting2/code.R
@@ -1,18 +1,25 @@
 library(readr)
 library(e1071)
 
-# Data extracting
-df <- read_delim("C:/Users/Kamil/Desktop/AirQualityUCI.csv", 
+#Data extracting
+df <- read_delim("AirQualityUCI.csv", 
                  delim = ";", escape_double = FALSE, 
                  locale = locale(date_format = "%d/%m/%Y", 
                                  decimal_mark = ",", grouping_mark = "."), 
                  na = "empty", trim_ws = TRUE)
 
 
-# Replace the -200 placeholder with NA for the column you want to plot
-df$'RH'[df$'RH' == -200] <- NA
+#Replace the -200 placeholder with NA
+df[df == -200] <- NA
 
-# Generate the Q-Q Plot
+#Generate the Q-Q Plot
+qqnorm(df$'T', 
+       main = "Normal Q-Q Plot for T", 
+       xlab = "Theoretical Quantiles", 
+       ylab = "Sample Quantiles", 
+       col = "steelblue", 
+       pch = 1)
+
 qqnorm(df$'RH', 
        main = "Normal Q-Q Plot for RH", 
        xlab = "Theoretical Quantiles", 
@@ -20,9 +27,35 @@ qqnorm(df$'RH',
        col = "steelblue", 
        pch = 1)
 
-# Add the reference line to see how close it is to a normal distribution
-qqline(df$'RH', col = "red", lwd = 2)
+qqnorm(df$'CO(GT)', 
+       main = "Normal Q-Q Plot for CO(GT)", 
+       xlab = "Theoretical Quantiles", 
+       ylab = "Sample Quantiles", 
+       col = "steelblue", 
+       pch = 1)
 
-# Calculate skewness
+qqnorm(df$'PT08.S1(CO)', 
+       main = "Normal Q-Q Plot for PT08.S1(CO)", 
+       xlab = "Theoretical Quantiles", 
+       ylab = "Sample Quantiles", 
+       col = "steelblue", 
+       pch = 1)
+
+#Add the reference line to see how close it is to a normal distribution
+qqline(df$'T', col = "red", lwd = 2)
+qqline(df$'RH', col = "red", lwd = 2)
+qqline(df$'CO(GT)', col = "red", lwd = 2)
+qqline(df$'PT08.S1(CO)', col = "red", lwd = 2)
+
+#Calculate skewness
+t_skewness <- skewness(df$T, na.rm = TRUE)
+cat("The skewness for Temperature (T) is:", t_skewness, "\n")
+
 rh_skewness <- skewness(df$RH, na.rm = TRUE)
 cat("The skewness for Relative Humidity (RH) is:", rh_skewness, "\n")
+
+COGT_skewness <- skewness(df$CO(GT), na.rm = TRUE)
+cat("The skewness for CO(GT) is:", COGT_skewness, "\n")
+
+pt_skewness <- skewness(df$PT08.S1(CO), na.rm = TRUE)
+cat("The skewness for PT08.S1(CO) is:", pt_skewness, "\n")
